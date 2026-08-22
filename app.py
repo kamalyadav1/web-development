6d29db6ac51d3d2159c1e96e921fcb44d204acba
kamalyadav1 <ky0592@srmist.edu.in>
2026-08-22 17:53:52 +0530

    Update app.py
    changing in the app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+from kafka import KafkaProducer
+import os
+import json
 
 app = Flask(__name__)
+
+# Aiven Kafka connection
+producer = KafkaProducer(
+    bootstrap_servers=os.environ["KAFKA_BOOTSTRAP_SERVERS"],
+    security_protocol=os.environ["KAFKA_SECURITY_PROTOCOL"],
+    sasl_mechanism=os.environ["KAFKA_SASL_MECHANISM"],
+    sasl_plain_username=os.environ["KAFKA_USERNAME"],
+    sasl_plain_password=os.environ["KAFKA_PASSWORD"],
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
 
 @app.route('/')
 def homes():
     return render_template('home.html')
 
-# Fixed the template name to match the route purpose
 @app.route('/sig')
 def signup():
     return render_template('signin.html')
